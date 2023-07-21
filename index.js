33dcb35fdc1abc9d3402e1f0406ff555a9c3dc97
HabchiSidAli <sidalihabchi96@gmail.com>
2023-07-21 03:06:48 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let array = [];
+
+function writeCards (names, type) {
+    for (let i = 0; names.length > i; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
+        if (names.length == i + 1){
+            return(array);
+        }
+    }
+}
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        --number;
+    }
+}
